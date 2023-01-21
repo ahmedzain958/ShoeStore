ee64387db97f8f14b1f7881778d2afd2b33f327d
Zain <ahmed.zaineldin@vodafone.com>
2023-01-21 18:27:48 +0200

    first shoe store commit with the first fragment

---
+++ app/src/main/java/com/udacity/shoestore/login/LoginFragment.kt
@@ -0,0 +1,28 @@
+package com.udacity.shoestore.login
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.udacity.shoestore.R
+import com.udacity.shoestore.databinding.FragmentLoginBinding
+
+
+class LoginFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View? {
+        // Inflate the layout for this fragment
+        val binding: FragmentLoginBinding = DataBindingUtil.inflate(layoutInflater, R.layout.fragment_login, container,false)
+        return binding.root
+    }
+}
